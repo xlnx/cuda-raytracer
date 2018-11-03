7ab3182d16b1aba96102dc4a15da513214a1dc9f
xlnx <qwq@koishi.top>
2018-11-03 08:46:50 +0800

    d:::

--- src/core/dev.hpp
+++ src/core/dev.hpp
@@ -39,6 +39,9 @@ struct Device
 
 namespace trait
 {
+struct dummy
+{
+};
 
 template <typename T, typename = void>
 struct is_host_callable: std::integral_constant<bool, false>
@@ -61,6 +64,14 @@ struct is_device_callable: std::integral_constant<bool, true>
 };
 
 }
+
+template <typename T>
+struct Require : typename std::conditional<
+				   std::is_base_of<Host, T>::value, Host, trait::dummy>::type,
+				 typename std::conditional<
+				   std::is_base_of<Device, T>::value, Device, trait::dummy>::type
+{
+};
 
 }  // namespace core
 
